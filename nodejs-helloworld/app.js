33076d43c84cb7942cba3538794ef012a898c346
visagan-tech <73689223+visagan-tech@users.noreply.github.com>
2020-11-03 14:52:37 +0400

    Update app.js

--- nodejs-helloworld/app.js
+++ nodejs-helloworld/app.js
@@ -2,10 +2,10 @@ var express = require('express');
 app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World!\n');
+  res.send('Hello Shivagami World!\n');
 });
 
 app.listen(8080, function () {
-  console.log('Example app listening on port 8080!');
+  console.log('Example app Shivagami listening on port 8080!');
 });
 
